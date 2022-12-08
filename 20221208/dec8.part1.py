ea24bf6a72b0bd5eb0112cc977cbe6270830a495
MX61LW <alain.vizzini@nnlife.co.jp>
2022-12-08 16:21:44 +0900

    dec8 part 1 ok

--- 20221208/dec8.part1.py
+++ 20221208/dec8.part1.py
@@ -1,6 +1,54 @@
 import io
 import sys
 
+
+def isVisible(mx, x, y):
+    #1: borders?
+    if x == 0 or y == 0 or x == len(mx[0])-1 or y == len(mx) -1:
+        return True
+
+    #2: top?
+    safe = False
+    for r in range(y):
+        if mx[r][x] >= mx[y][x]:
+            safe = True
+    if not safe:
+        print (f"({x}, {y}) visible from TOP")
+        return True 
+
+    #3: bottom?
+    safe = False
+    for r in range(len(mx)-1, y, -1):
+        if mx[r][x] >= mx[y][x]:
+            safe = True
+    if not safe:
+        print (f"({x}, {y}) visible from BOTTOM")
+        return True 
+
+    #4: left?
+    safe = False
+    for c in range(x):
+        if mx[y][c] >= mx[y][x]:
+            safe = True
+    if not safe:
+        print (f"({x}, {y}) visible from LEFT")
+        return True 
+
+    #5: right?
+    safe = False
+    for c in range(len(mx[0])-1, x, -1):
+        if mx[y][c] >= mx[y][x]:
+            safe = True
+    if not safe:
+        print (f"({x}, {y}) visible from RIGHT")
+        return True 
+
+    #you're well hidden
+    return False
+
+
+
+##############################################################################################################
 filename = "input.txt"
 llimit = -1
 if len(sys.argv) < 2:
@@ -13,8 +61,7 @@ if len(sys.argv) >= 3:
     print(f"INFO: 2nd param is limit of lines to process, will process only the first {sys.argv[2]} lines")
     llimit = int(sys.argv[2])
 
-
-
+mx = []
 lcount = 0
 with io.open(filename, "r") as f:
     while True:
@@ -31,5 +78,16 @@ with io.open(filename, "r") as f:
         #process!
         l = l.strip()
 
-        #edit me v v v v v  
-        print(f"{lcount}: {l}") 
+        #add the line as a list of char (not string) with the [*_string_] syntax
+        mx.append([*l])
+
+print (mx)
+
+totalVisible = 0
+for x in range(len(mx[0])):
+    for y in range(len(mx)):
+        visible = isVisible(mx,x,y)
+        #print (f"({x}, {y}) visible")
+        totalVisible = totalVisible + (1 if visible else 0)
+
+print (f"Total visible is {totalVisible}.")
