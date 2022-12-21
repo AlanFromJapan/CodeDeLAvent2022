8d6ddd80c39eec8b6162168990d0f0fc3fe00f97
MX61LW <alain.vizzini@nnlife.co.jp>
2022-12-21 14:32:51 +0900

    Day21 part 1 ok, at last a simple parser tree!

---
+++ 20221221/dec21.part1.py
@@ -0,0 +1,126 @@
+import io
+import sys
+
+filename = "input.txt"
+llimit = -1
+DBG = True
+
+
+class Node:
+    type = "0"
+    left = 0
+    right = 0
+    name = "?"
+
+    def __init__(self, type, name, left, right = 0) -> None:
+        self.type = type
+        self.left = left
+        self.right = right
+        self.name = name
+
+    def value(self):
+        if self.type == "0":
+            #direct
+            return int(self.left)
+        else:
+            if self.type == "+":
+                return self.left.value() + self.right.value() 
+            elif self.type == "-":
+                return self.left.value() - self.right.value() 
+            elif self.type == "*":
+                return self.left.value() * self.right.value() 
+            elif self.type == "/":
+                return self.left.value() / self.right.value() 
+    
+    def __str__(self) -> str:
+        return f"[Node n={self.name} t={self.type} l={self.left} r={self.right}]"
+
+    def buildTree(self, dic):
+        if self.type == "0":
+            return self #done
+        
+        if isinstance(self.left, str):
+            if DBG:
+                print(f"Node {self.name} search for left {self.left}")
+            self.left = dic[self.left]
+            self.left.buildTree(dic)
+        
+        if isinstance(self.right, str):
+            if DBG:
+                print(f"Node {self.name} search for right {self.right}")
+            self.right = dic[self.right]
+            self.right.buildTree(dic)
+        
+        return self
+
+#node from job (not int) but with NAMES as L/R
+def nodeFromFormula (job, oper):
+    formula = job.split(oper)
+    node = Node(oper, n, formula[0].strip(), formula[1].strip())
+    return node
+
+
+if len(sys.argv) < 2:
+    print("INFO: no filename passed as param, defaulting to 'input.txt'")
+else:
+    print(f"INFO: 1st param is filename, will process '{sys.argv[1]}'")
+    filename = str(sys.argv[1])
+
+if len(sys.argv) >= 3:
+    print(f"INFO: 2nd param is limit of lines to process, will process only the first {sys.argv[2]} lines")
+    llimit = int(sys.argv[2])
+
+
+
+lcount = 0
+root = None
+nodes = {}
+with io.open(filename, "r") as f:
+    while True:
+        l = f.readline()
+        lcount = lcount +1
+
+        if not l:
+            #finished!
+            break
+
+        if llimit > 0 and lcount > llimit:
+            break
+
+        #process!
+        l = l.strip()
+
+        #edit me v v v v v  
+        #print(f"{lcount}: {l}") 
+        n = l.split(":")[0]
+        job = l.split(":")[1].strip()
+        node = None
+        if "+" in job:
+            node = nodeFromFormula(job, "+")
+        elif "-" in job:
+            node = nodeFromFormula(job, "-")
+        elif "*" in job:
+            node = nodeFromFormula(job, "*")
+        elif "/" in job:
+            node = nodeFromFormula(job, "/")
+        else:
+            #Should be an int
+            node = Node("0", n, int(job))
+        
+        if node == None:
+            raise "Shouldn't happen!"
+        
+        node.name = n
+        if node.name == "root":
+            root = node
+
+        #store
+        nodes[n] = node
+
+#print(root)
+
+print("Building tree")
+root = root.buildTree(nodes)
+#print(root)
+
+print(f"Root val = {root.value()}")
